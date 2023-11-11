cca31c332cab8ba09dc467117c554229d882a66e
Oliver Hidalgo <ohgovilla@gmail.com>
2023-11-10 21:11:06 -0600

    ADD - Initial Repo - odyssey part 1 finished

---
+++ server/src/schema.js
@@ -0,0 +1,34 @@
+const gql = require("graphql-tag");
+
+const typeDefs = gql`
+  type Query {
+    "Query to get tracks array for the homepage grid"
+    tracksForHome: [Track!]!
+  }
+
+  "A track is a group of Modules that teaches about a specific topic"
+  type Track {
+    id: ID!
+    "The tracks title"
+    title: String!
+    "The tracks main author"
+    author: Author!
+    "The tracks main illustration to display in track card or track page detail"
+    thumbnail: String
+    "The tracks approximate length to complete, in minutes"
+    length: Int
+    "The number of modules this track contains"
+    modulesCount: Int
+  }
+
+  "Author of a complete Track"
+  type Author {
+    id: ID!
+    "Author's first and last name"
+    name: String!
+    "Author's profile picture url"
+    photo: String
+  }
+`;
+
+module.exports = typeDefs;
